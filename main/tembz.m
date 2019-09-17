ed445cba551b54cdd7866b6777a244fa693ac8ca
Alexander von Lühmann <avolu@bu.edu>
2019-09-17 19:43:12 -0400

    fix

--- main/tembz.m
+++ main/tembz.m
@@ -2,7 +2,7 @@ function [sig] = tembz(sig, param)
 %temporally embeds and zscores a signal using the parameters from param struct
 aux_emb = sig;
 for i=1:param.NumOfEmb
-    aux=circshift(aux_sigs, i*param.tau, 1);
+    aux=circshift(sig, i*param.tau, 1);
     aux(1:2*i,:)=repmat(aux(2*i+1,:),2*i,1);
     aux_emb=[aux_emb aux];
 end
