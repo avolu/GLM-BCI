5ccf135bc264966d3a37f0556f6881401eec1339
Alexander von Lühmann <avolu@bu.edu>
2019-09-18 11:18:40 -0400

    save data and split off  plot + classify

---
+++ main/classify.m
@@ -0,0 +1,22 @@
+clear all;
+
+malexflag = 0; % user flag
+if malexflag
+    %Meryem
+    path.code = 'C:\Users\mayucel\Documents\PROJECTS\CODES\GLM-BCI'; addpath(genpath(path.code)); % code directory
+    path.dir = 'C:\Users\mayucel\Google Drive\tCCA_GLM_PAPER\FB_RESTING_DATA'; % data directory
+    path.save = path.code; % save directory
+    
+    %Meryem Laptop
+    %     path.code = 'C:\Users\m\Documents\GitHub\GLM-BCI'; addpath(genpath(path.code)); % code directory
+    %     path.dir = 'C:\Users\m\Documents\tCCA_GLM_PAPER\FB_RESTING_DATA'; % data directory
+    %     path.save = 'C:\Users\m\Documents\tCCA_GLM_PAPER\FB_RESTING_DATA'; % save directory
+else
+    %Alex
+    path.code = 'D:\Office\Research\Software - Scripts\Matlab\GLM-BCI'; addpath(genpath(path.code)); % code directory
+    path.dir = 'C:\Users\avolu\Google Drive\tCCA_GLM_PAPER\FB_RESTING_DATA'; % data directory
+    path.save = path.code; % save directory
+end
+
+load([path.save '\FV_results.mat'])
+
--- main/run_CCA.m
+++ main/run_CCA.m
@@ -1,11 +1,11 @@
 clear all;
 
-malexflag = 1; % user flag
+malexflag = 0; % user flag
 if malexflag
     %Meryem
     path.code = 'C:\Users\mayucel\Documents\PROJECTS\CODES\GLM-BCI'; addpath(genpath(path.code)); % code directory
     path.dir = 'C:\Users\mayucel\Google Drive\tCCA_GLM_PAPER\FB_RESTING_DATA'; % data directory
-    path.save = 'C:\Users\mayucel\Google Drive\tCCA_GLM_PAPER'; % save directory
+    path.save = path.code; % save directory
     
     %Meryem Laptop
     %     path.code = 'C:\Users\m\Documents\GitHub\GLM-BCI'; addpath(genpath(path.code)); % code directory
@@ -15,7 +15,7 @@ else
     %Alex
     path.code = 'D:\Office\Research\Software - Scripts\Matlab\GLM-BCI'; addpath(genpath(path.code)); % code directory
     path.dir = 'C:\Users\avolu\Google Drive\tCCA_GLM_PAPER\FB_RESTING_DATA'; % data directory
-    path.save = 'C:\Users\avolu\Google Drive\tCCA_GLM_PAPER'; % save directory
+    path.save = path.code; % save directory
 end
 
 % #####
@@ -31,7 +31,7 @@ sbjfolder = {'Subj33','Subj34','Subj36','Subj37','Subj38','Subj39', 'Subj40', 'S
 
 %% Options/Parameter Settings
 rhoSD_ssThresh = 15;  % mm
-flag_save = 0;
+flag_save = 1;
 flag_conc = 1; % if 1 CCA inputs are in conc, if 0 CCA inputs are in intensity
 % results eval parameters
 eval_param.HRFmin = -2;
@@ -299,147 +299,11 @@ for sbj = 1:numel(sbjfolder) % loop across subjects
     clear vars AUX d d0 d_long d0_long d_short d0_short t s REG_trn ADD_trn
 end
 
-% %% save data
-%     if flag_save
-%         disp(['saving sbj ' num2str(sbj) '...'])
-%         save([path.save sfoldername '\results_sbj' num2str(sbj) '.mat'], 'DET_SS', 'DET_CCA', 'pval_SS', 'pval_CCA', 'ROCLAB', 'MSE_SS', 'MSE_CCA', 'CORR_SS', 'CORR_CCA', 'nTrials');
-%     end
-
-
-%% Sort through results and append
-F_Raw_Hrf=[];
-F_Raw_NoHrf=[];
-F_SS_Hrf=[];
-F_SS_NoHrf=[];
-F_CCA_Hrf=[];
-F_CCA_NoHrf=[];
-for sbj = 1:numel(sbjfolder)
-    % channel indices that have or dont have gt HRF
-    idxChHrf = lstHrfAdd{sbj}(:,1);
-    idxChNoHrf = arrayfun(@(x) find(lstLongAct{sbj}==x,1),squeeze(lstHrfAdd{sbj}(:,1)));
-    % number of available channels
-    sHrf = size(FMdc{sbj}(:,:,idxChHrf,:));
-    sNoHrf = size(FMdc{sbj}(:,:,idxChNoHrf,:));
-    % extract and append data, new dimension is F x C x I,
-    % where F: # of Features, C: # Number of Chromophores, I: # of all
-    % trials (epochs*channels)
-    F_Raw_Hrf = cat(3, F_Raw_Hrf, reshape(FMdc{sbj}(:,:,idxChHrf,:),numel(clab),3,sHrf(3)*sHrf(4)));
-    F_Raw_NoHrf = cat(3, F_Raw_NoHrf, reshape(FMdc{sbj}(:,:,idxChNoHrf,:),numel(clab),3,sNoHrf(3)*sNoHrf(4)));
-    F_SS_Hrf = cat(3, F_SS_Hrf, reshape(FMss{sbj}(:,:,idxChHrf,:),numel(clab),3,sHrf(3)*sHrf(4)));
-    F_SS_NoHrf = cat(3, F_SS_NoHrf, reshape(FMss{sbj}(:,:,idxChNoHrf,:),numel(clab),3,sNoHrf(3)*sNoHrf(4)));
-    F_CCA_Hrf = cat(3, F_CCA_Hrf, reshape(FMcca{sbj}(:,:,idxChHrf,:),numel(clab),3,sHrf(3)*sHrf(4)));
-    F_CCA_NoHrf = cat(3, F_CCA_NoHrf, reshape(FMcca{sbj}(:,:,idxChNoHrf,:),numel(clab),3,sNoHrf(3)*sNoHrf(4)));
-    
+%% save data
+if flag_save
+    disp('saving data...')
+    save([path.save '\FV_results.mat'], 'FMdc', 'FMss', 'FMcca', 'FWss', 'FWcca', 'TTM');
 end
-
-%% Paired T-Tests
-for ff = 1:9
-    for cc=1:3
-        [h_co(ff,cc,1),p_co(ff,cc,1)]= ttest(squeeze(F_Raw_Hrf(ff,cc,:)),squeeze(F_SS_Hrf(ff,cc,:)));
-        [h_co(ff,cc,2),p_co(ff,cc,2)]= ttest(squeeze(F_Raw_Hrf(ff,cc,:)),squeeze(F_CCA_Hrf(ff,cc,:)));
-        [h_co(ff,cc,3),p_co(ff,cc,3)]= ttest(squeeze(F_SS_Hrf(ff,cc,:)),squeeze(F_CCA_Hrf(ff,cc,:)));
-    end
-end
-
-%% (Box)Plot results (normal metrics)
-figure
-labels = {'No GLM', 'GLM SS', 'GLM tCCA'};
-chrom = {' HbO', ' HbR'};
-% for all features
-for ff=1:9
-    % for both chromophores
-    for cc=1:2
-        subplot(2,9,(cc-1)*9+ff)
-        xtickangle(35)
-        hold on
-        %% boxplots
-        % with cca
-        if flag_plotCCA
-            boxplot([squeeze(F_Raw_Hrf(ff,cc,:)), squeeze(F_SS_Hrf(ff,cc,:)), squeeze(F_CCA_Hrf(ff,cc,:))], 'labels', labels)
-            H=sigstar({[1,2],[1,3],[2,3]},squeeze(p_co(ff,cc,1:3)));
-        else
-            % without cca
-            boxplot([squeeze(F_Raw_Hrf(ff,cc,:)), squeeze(F_SS_Hrf(ff,cc,:))], 'labels', labels(1:2))
-            H=sigstar({[1,2]},squeeze(p_co(ff,cc,1)));
-        end
-        
-        % ground truth
-        if ff<8
-            hAx=gca;                                   % retrieve the axes handle
-            %xtk=hAx.XTick;                             % and the xtick values to plot() at...
-            xtk = [0.5, 1, 2, 3, 3.5];
-            hold on
-            if ff==5
-                FVgt.x(ff,cc) = 6; % set time to peak to 6 seconds (due to gt hrf plateau)
-            end
-            hL=plot(xtk, ones(numel(xtk))*FVgt.x(ff,cc),'--g');
-        end
-        if ff<5
-            ylabel('\muMol')
-        end
-        if ff==5
-            ylabel('sec')
-        end
-        if ff==9
-            ylabel('Mol')
-        end
-        
-        title([clab{ff} chrom{cc}])
-    end
-end
-
-
-%% (Box)Plot results (metric errors)
-figure
-labels = {'No GLM', 'GLM SS', 'GLM tCCA'};
-chrom = {' HbO', ' HbR'};
-% for all features
-for ff=1:9
-    % for both chromophores
-    for cc=1:2
-        subplot(2,9,(cc-1)*9+ff)
-        xtickangle(35)
-        hold on
-        %% boxplots
-        if ff==5
-            FVgt.x(ff,cc) = 6; % set time to peak to 6 seconds (due to gt hrf plateau)
-        end
-        
-        if ff<8
-            % without GLM, with GLM+SS, with GLM+CCA
-            if flag_plotCCA
-                boxplot([abs(squeeze(F_Raw_Hrf(ff,cc,:))-FVgt.x(ff,cc)), abs(squeeze(F_SS_Hrf(ff,cc,:))-FVgt.x(ff,cc)), abs(squeeze(F_CCA_Hrf(ff,cc,:))-FVgt.x(ff,cc))], 'labels', labels)
-                H=sigstar({[1,2],[1,3],[2,3]},squeeze(p_co(ff,cc,1:3)));
-            else
-                boxplot([abs(squeeze(F_Raw_Hrf(ff,cc,:))-FVgt.x(ff,cc)), abs(squeeze(F_SS_Hrf(ff,cc,:))-FVgt.x(ff,cc))], 'labels', labels(1:2))
-                H=sigstar({[1,2]},squeeze(p_co(ff,cc,1)));
-            end
-            title(['ERR ' clab{ff} chrom{cc}])
-        else
-            if flag_plotCCA
-                boxplot([squeeze(F_Raw_Hrf(ff,cc,:)), squeeze(F_SS_Hrf(ff,cc,:)), squeeze(F_CCA_Hrf(ff,cc,:))], 'labels', labels)
-                H=sigstar({[1,2],[1,3],[2,3]},squeeze(p_co(ff,cc,1:3)));
-            else
-                boxplot([squeeze(F_Raw_Hrf(ff,cc,:)), squeeze(F_SS_Hrf(ff,cc,:))], 'labels', labels(1:2))
-                H=sigstar({[1,2]},squeeze(p_co(ff,cc,1)));
-            end
-            
-            title([clab{ff} chrom{cc}])
-        end
-        
-        
-        if ff<5
-            ylabel('\muMol')
-        end
-        if ff==5
-            ylabel('sec')
-        end
-        if ff==9
-            ylabel('\muMol')
-        end
-    end
-end
-
 
 toc;
 
